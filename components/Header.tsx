da8673b3eeeec65a4a3053f625639dcff8b33d6a
Anteneh Atre <anteneh.atre@gmail.com>
2025-02-14 00:49:28 +0300

    Implementing ligt and dark mode with shadcn

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,19 @@
-
+import DarkModeToggle from "./DarkModeToggle";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="sticky top-0 left-0 right-0 z-50 bg-background/80 backdrop-blur-sm border-b border-border">
+      <div className="container mx-auto px-4">
+        <div>
+          {/* Left */}
+          <div className="flex items-center gap-4"></div>
+          {/* Right */}
+          <div className="flex items-center space-x-2 md:space-x-4"></div>
+        </div>
+        <DarkModeToggle />
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
